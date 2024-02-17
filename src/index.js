ace0aa5d4cb7a8787d474dc5d2e43af48fe9787c
jakobap <jakobpoerschmann@gmail.com>
2024-02-17 16:43:46 +0200

    refactor directories

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Index.css';
-
-ReactDOM.render(<App />, document.getElementById("root"));
