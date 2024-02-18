8f965c9603aa7851611f04a6a0dd8d1e05a2a47b
jakobap <jakobpoerschmann@gmail.com>
2024-02-18 23:16:56 +0200

    chat functional

--- website/src/components/Chat/ActionProvider.jsx
+++ website/src/components/Chat/ActionProvider.jsx
@@ -3,37 +3,39 @@ import React from 'react';
 
 const ActionProvider = ({ createChatBotMessage, setState, children }) => {
 
-
-  const LLMServerRequest = (rawString) => {
+  const LLMServerRequest =  async (rawString) => {
     const formData = new FormData();
     formData.append('query', rawString);
-
-    fetch('https://hello-jp-llmserver-nbzldk2rfa-ew.a.run.app/generate_chat_response', {
-      method: 'POST',
-      body: formData
-    })
-      .then(response => {
+ 
+    try {
+        const response = await fetch('https://hello-jp-llmserver-stag-nbzldk2rfa-ew.a.run.app/generate_chat_response', {
+            method: 'POST',
+            body: formData
+        });
+ 
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+            throw new Error('Network response was not ok');
         }
-        return response.text(); 
-      })
-      .then(data => {
-        console.log('Response from server:', data);
-        return data;
-      })
-      .catch(error => {
+ 
+        const textData = await response.text(); 
+        const responseParsed = JSON.parse(textData)['text']; 
+        return responseParsed;
+ 
+    } catch (error) {
         console.error('Error during POST request:', error);
-      });
-  };
+        throw error; // Allow handling at a higher level 
+    }
+ };
 
-  const GenerateLLMResponse = (user_message) => {
-    const prompt = user_message;
-    const model_response = LLMServerRequest(prompt);
-    return model_response
-  };
+  const GenerateLLMResponse = async (user_message) => {
+      const prompt = user_message;
+      console.log('Prompt:', prompt);
+      const model_response = await LLMServerRequest(prompt);
+      return model_response;
+    };
 
   const SendResponse = (response_to_send) => {
+    console.log('Response to send:', response_to_send);
     const botMessage = createChatBotMessage(response_to_send);
     setState((prev) => ({
       ...prev,
--- website/src/components/Chat/MessageParser.jsx
+++ website/src/components/Chat/MessageParser.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
 
-const MessageParser = ({ children, actions }) => {
-  const parse = (message) => {
-    console.log(message);
-
-    const model_response = actions.GenerateLLMResponse(message);
-
-    actions.SendResponse(model_response);
-
+function MessageParser ({ children, actions }) {
+  const parse = async (message) => {
+    console.log('Incoming message:', message);
+    const response = await actions.GenerateLLMResponse(message);
+    console.log('Outgoing message:', response);
+    actions.SendResponse(response);
+    return response
   };
-
   return (
     <div>
       {React.Children.map(children, (child) => {
